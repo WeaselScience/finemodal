14a143884d6697a4ab27c0c1becd7ac538013dff
Algirdas <mrinter5@gmail.com>
2016-09-14 22:03:44 +0000

    derp

--- packages/finemodal/package.js
+++ packages/finemodal/package.js
@@ -2,13 +2,13 @@ Package.describe({
     name: 'apotex:finemodal',
     version: '0.0.1',
     summary: 'Yet another (again) implementation of the Boostrap Modal',
-    git: 'https://github.com/apotex/finemodal',
-    documentation: 'README.md'
+    git: 'https://github.com/weasel-science/finemodal',
+    documentation: '../../README.md'
 });
 
 Package.onUse(function(api) {
     api.versionsFrom('1.4.1.1');
-    
+
     api.use([
         'ecmascript',
         'check',
@@ -18,13 +18,13 @@ Package.onUse(function(api) {
         'underscore',
         'templating'
     ], 'client');
-    
+
     api.addFiles([
         'FineModal.js',
         'FineModalUtil.js',
         'helpers.html',
         'Stack.js'
     ], 'client');
-    
+
     api.mainModule('main.js', 'client');
 });
